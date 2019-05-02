fbfa6b5c0c06de3311a86f8e80c7033f705a43db
Artucuno1234 <Articuno1234@users.noreply.github.com>
2019-05-02 17:01:54 +1000

    [NEW API FORMAT] read

--- discord.py/async/0.16.12/bot.py
+++ discord.py/async/0.16.12/bot.py
@@ -1,33 +1,39 @@
-# An Example from the MDBL Orginisaton
-# https://github.com/MegaDiscordBotList/
-# https://mdbl.surge.sh/
-
 import discord
 from discord.ext import commands
 from discord.ext.commands import Bot
-token = ''
-prefix = ''
-
+import logging
+ownerid = ''
 logging.basicConfig(level=logging.INFO) # Configurates the logger
 logger = logging.getLogger('discord')
-bot = Bot(command_prefix=prefix) # Sets the client and sets the prefix
+bot = Bot(command_prefix='!') # Sets the client and sets the prefix
 
 @bot.event
 async def on_ready():
+    cmds = len(bot.commands)
+    users = len(set(bot.get_all_members()))
     servers = len(bot.servers)
-    users = len(set(bot.get_all_members())
+    channels = len([c for c in bot.get_all_channels()])
     # MDBL Status Update
     channel = bot.get_channel("567208400891543552")
-    await bot.send_message(channel, "@!update {} {}".format(servers, users))
-    print("MDBL: Updated bot status (Guilds: {} | Users: {})".format(servers, users))
-    
+    await bot.send_message(channel, "@!update {} {} {} {}".format(servers, users, channels, commands))
+    msg = await bot.wait_for_message(content='API = True')
+    print("MDBL: Updated bot status (Guilds: {} | Users: {} | Channels: {} | Commands: {})".format(servers, users, channels, commands))
+
 @bot.command(pass_context=True)
 async def update(ctx):
+    """Update status for MDBL"""
     # MDBL Status Update
+    cmds = len(bot.commands)
+    users = len(set(bot.get_all_members()))
     servers = len(bot.servers)
-    users = len(set(bot.get_all_members())
-    channel = bot.get_channel("567208400891543552")
-    await bot.send_message(channel, "@!update {} {}".format(servers, users))
-    print("MDBL: Updated bot status (Guilds: {} | Users: {})".format(servers, users))
-    
-bot.run(token)
+    channels = len([c for c in bot.get_all_channels()])
+    author = ctx.message.author
+    if author.id == ownerid:
+        channel = bot.get_channel("567208400891543552")
+        await bot.send_message(channel, "@!update {} {} {} {}".format(servers, users))
+        msg = await bot.wait_for_message(content='API = True')
+        print("MDBL: Updated bot status (Guilds: {} | Users: {} | Channels: {} | Commands: {})".format(servers, users, channels, commands))
+    else:
+        await bot.say("You are not the owner of this bot)
+bot.run('token')
+ 
